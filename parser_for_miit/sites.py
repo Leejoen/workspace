61d846551b15744cb3e5b6329a1f2d6e21ee89a4
Alina Karpova <92012078+Leejoen@users.noreply.github.com>
2023-10-14 21:12:28 +0000

    new commit

---
+++ parser_for_miit/sites.py
@@ -0,0 +1,12 @@
+sites = {'usurt':'https://www.usurt.ru/main-news','uvauga':'https://uvauga.ru','spbguga':'https://spbguga.ru/category/news/',
+         'stu':'http://www.stu.ru/news/','ssuwt':'https://www.ssuwt.ru/?limit=12&limitstart=1','rgups':'https://www.rgups.ru/news/',
+         'pgups':'https://pgups.ru/news/','omgups':'https://www.omgups.ru/news/',
+         'irgups':'https://www.irgups.ru/news','dvgups':'https://dvgups.ru/press-main-page','gumrf':'https://gumrf.ru/news/',
+         'samgups':'https://www.samgups.ru/?ysclid=lb   9qikik0245199152&PAGEN_1=1',
+         'aumsu':'http://pk.aumsu.ru/novosti','msun':'https://www.msun.ru/ru/news/arch'}
+
+#'mstuca':'https://www.mstuca.ru/about_the_university/news/',
+#'vsuwt':'https://vsuwt.ru/novosti/novosti-universiteta/',
+
+
+
